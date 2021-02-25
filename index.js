945a481a30ad4b9c05acecccc113d32b98b15368
Francesco Francomano <francesco.francomano@mia-platform.eu>
2021-02-25 22:16:37 +0100

    almost done

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const path = require('path')
 const fileParser = require('./src/fileParser')
-const { pizzasManager } = require('./src/managers/pizzasManager')
 const fs = require('fs-extra')
 const get = require('lodash.get')
 const set = require('lodash.set')
@@ -13,10 +12,10 @@ const files = [
   'c',
   'd',
   'e',
-  'f',
+  'f'
 ]
 
-function parseFile(fileName) {
+function parseFile (fileName) {
   const { rows } = fileParser(
     path.join(__dirname, `./fixtures/${fileName}.txt`),
     { splitInRows: true }
@@ -25,15 +24,17 @@ function parseFile(fileName) {
   const [executionTime, interceptionsCount, streetsCount, carsCount, carScore] = header.split(' ').filter(x => x)
   const rawStreets = others.slice(0, streetsCount)
   const rawCars = others.slice(streetsCount)
+  // eslint-disable-next-line eqeqeq
   if (rawCars.length != carsCount) {
-    console.log("cars.length != carsCount", rawCars.length, carsCount)
+    console.log('cars.length != carsCount', rawCars.length, carsCount)
   }
   const { streets, interceptions } = rawStreets.reduce((acc, string) => {
     const [idStart, idEnd, streetName, travelTime] = string.split(' ')
     acc.streets[streetName] = {
       idStart: parseInt(idStart, 10),
       idEnd: parseInt(idEnd, 10),
-      travelTime: parseInt(travelTime, 10)
+      travelTime: parseInt(travelTime, 10),
+      carFlow: 0
     }
 
     const getInStreets = get(acc, ['interceptions', idEnd, 'inStreets'], [])
@@ -44,11 +45,17 @@ function parseFile(fileName) {
     return acc
   }, { streets: {}, interceptions: {} })
 
-  if (interceptions.length != interceptionsCount) {
-    console.log("interceptions.length != interceptionsCount", interceptions.length, interceptionsCount)
+  // eslint-disable-next-line eqeqeq
+  if (Object.keys(interceptions).length != interceptionsCount) {
+    console.log('interceptions.length != interceptionsCount', Object.keys(interceptions).length, interceptionsCount)
   }
+
   const carPaths = rawCars.map(string => {
     const [streetsInPathsCount, ...streetsInPath] = string.split(' ')
+    for (const streetName of streetsInPath) {
+      streets[streetName].carFlow += 1
+    }
+
     return {
       streetsInPathsCount,
       streetsInPath
@@ -58,14 +65,15 @@ function parseFile(fileName) {
   return {
     streets,
     carPaths,
-    interceptions
+    interceptions,
+    executionTime: parseInt(executionTime, 10)
   }
 }
 
 const start = () => {
   files.forEach(fileName => {
     // ********* FILE PARSING ***********
-    const { carPaths, streets, interceptions } = parseFile(fileName)
+    const { carPaths, streets, interceptions, executionTime } = parseFile(fileName)
     const interceptionsCount = Object.keys(interceptions).length
     // console.log('carPaths', carPaths)
     // console.log('streets', streets)
@@ -73,20 +81,28 @@ const start = () => {
     // console.log('interceptionsCount', interceptionsCount)
 
     // ******** OUTPUT WRITING ***********
-    const result = [interceptionsCount]
+    // TODO: da fixare con un count. attualmente è sbagliato
+    const result = []
+    let schedulesCount = 0
     for (const interceptionId in interceptions) {
       const { inStreets, outStreets } = interceptions[interceptionId]
-      const schedule = [interceptionId]
-      schedule.push(inStreets.length)
-      schedule.push(inStreets.map(name => `${name} 1`))
-      result.push(...schedule)
-    }
-    const output = result.join().replace(/,/g, '\n')
-    fs.outputFileSync(`./results/${fileName}.out`, output, { encoding: 'utf-8' })
+      const schedule = []
+      for (const street of inStreets) {
+        const lightDuration = streets[street].carFlow < executionTime
+          ? streets[street].carFlow
+          : executionTime
+        if (lightDuration !== 0) {
+          schedule.push(`${street} ${lightDuration}`)
+        }
+      }
 
-    // const result = pizzasManager({ pizzasNumber: executionTime, t2: interceptionNumber, t3, t4: carNumber }, others)
-    // const output = result.join().replace(/,/g, '\n')
-    // fs.outputFileSync(`./results/${fileName}.out`, output, { encoding: 'utf-8' })
+      if (schedule.length > 0) {
+        schedulesCount++
+        result.push(interceptionId, schedule.length, ...schedule)
+      }
+    }
+    const output = schedulesCount + '\n' + result.join().replace(/,/g, '\n')
+    fs.outputFileSync(`./results/${fileName}.out`, output, { encoding: 'utf-8' })
   })
 }
 
